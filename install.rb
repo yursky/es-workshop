03bcf4978ea47b5a971c1312ad0a20c4cac2eb39
Yuriy Minin <me@yuriy.io>
2017-10-23 16:25:02 -0500

    revert

--- install.rb
+++ install.rb
@@ -145,12 +145,6 @@ def install_es(install_path)
   end
 
   archive_file = File.join(install_path, File.basename(ES_INSTALL_URI.path))
-  if @os_type == 'windows'
-    archive_file.gsub('/', '\\')
-  end
-
-  print archive_file
-
   download(ES_INSTALL_URI, archive_file)
   unpack(archive_file, install_path)
 
